b757f268ce02fbeaaa1a3d87aaf005f1e3f280b6
Horst Gutmann <zerok@zerokspot.com>
2019-09-22 10:45:01 +0200

    Content now in Courier New

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
 const Gulp = require('gulp');
 const Sass = require('gulp-sass');
 const rename = require('gulp-rename');
-const merge = require('merge-stream');
 
-Gulp.task('sass', ['copy:normalize'], () => {
+function mainCSS() {
     return Gulp.src('static/sass/main.scss')
         .pipe(Sass())
         .pipe(Gulp.dest('static/css'));
-});
+}
 
-Gulp.task('copy:normalize', () => {
+function previewCSS() {
+    return Gulp.src('static/sass/preview.scss')
+        .pipe(Sass())
+        .pipe(Gulp.dest('static/css'));
+}
+
+exports.copyNormalized = function copyNormalized() {
     return Gulp.src('node_modules/normalize.css/normalize.css')
         .pipe(rename({basename: '_normalize', extname: '.scss'}))
         .pipe(Gulp.dest('static/sass'));
-});
+};
 
-Gulp.task('sass:watch', () => {
-    Gulp.watch('static/sass/*.scss', ['sass']);
-});
+exports.sass = Gulp.series(exports.copyNormalized, Gulp.parallel(mainCSS, previewCSS));
 
-Gulp.task('default', ['sass', 'sass:watch']);
+exports.sassWatch = function sassWatch() {
+    return Gulp.watch('static/sass/*.scss', Gulp.parallel(mainCSS, previewCSS));
+}
+exports.default = Gulp.series(exports.sass, exports.sassWatch);
