8acddd7c0ce9262f3633d0913b92be3b0597cda5
Horst Gutmann <zerok@zerokspot.com>
2019-10-13 12:44:58 +0200

    Ignore posts without a date when updating the index

--- cmd/blogsearch/cmd/mapping.go
+++ cmd/blogsearch/cmd/mapping.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type pageData struct {
 	ObjectID string `json:"objectID"`
 	File     string `json:"file"`
+	DateYear int    `json:"date_year"`
 }
 
 func buildMapping(root string) (map[string]string, error) {
@@ -23,6 +25,9 @@ func buildMapping(root string) (map[string]string, error) {
 			defer fp.Close()
 			if err := json.NewDecoder(fp).Decode(&page); err != nil {
 				return err
+			}
+			if strings.HasPrefix(page.ObjectID, "/weblog") && page.DateYear <= 1 {
+				return nil
 			}
 			result[page.File] = page.ObjectID
 		}
