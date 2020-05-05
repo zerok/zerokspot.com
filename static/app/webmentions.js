a50d7fb124fb3a40c5d5485762f04bb93e62bb8a
Horst Gutmann <zerok@zerokspot.com>
2020-05-05 09:56:54 +0200

    Render webmention comments

--- static/app/webmentions.js
+++ static/app/webmentions.js
@@ -15,10 +15,34 @@
             title.className = 'webmentions__title';
             mentions.forEach(mention => {
                 const li = document.createElement('li');
+                li.setAttribute('class', 'webmentions__item');
                 const a = document.createElement('a');
                 a.setAttribute('href', mention.source);
-                a.innerText = mention.source
+                if (mention.author_name && mention.type === 'comment') {
+                    a.innerText = mention.author_name
+                } else {
+                    a.innerText = mention.source
+                }
+                const icon = document.createElement('i');
+                if (mention.type === 'comment') {
+                    icon.setAttribute('class', 'far fa-comment');
+                } else {
+                    icon.setAttribute('class', 'far fa-link');
+                }
+                li.appendChild(icon);
                 li.appendChild(a);
+                if (mention.created_at) {
+                    const createdAt = document.createElement('span');
+                    createdAt.setAttribute('class', 'webmentions__item__date');
+                    createdAt.innerText = '@ ' + mention.created_at;
+                    li.appendChild(createdAt);
+                }
+                if (mention.content) {
+                    const bq = document.createElement('blockquote');
+                    bq.innerText = mention.content;
+                    bq.setAttribute('class', 'webmentions__item__comment');
+                    li.appendChild(bq);
+                }
                 listing.appendChild(li);
             });
             fragment.appendChild(title);
